dc74fe9424ab7fc22d144fccd25e5c2421628814
James Harvey <jamesharvey@jamess-air.lan>
2024-10-14 09:36:54 -0400

    testing

--- scripts/sockman.sh
+++ scripts/sockman.sh
@@ -169,7 +169,7 @@ function open_list_sockets_pane() {
   local pane_found="$(tmux select-pane -t "${pane_id}" && echo true)"
 
   if [[ -z "${pane_found}" ]]; then
-    local winid="$(tmux new-window -P bash -c 'source '"${CURRENT_DIR}"'/sockman.sh && list_sockets '"${session_name}"'')"
+    local winid="$(tmux new-window -P bash -c 'source '"${CURRENT_DIR}"'/sockman.sh && list_sockets '"${session_name}"'; sleep 5')"
 
     local session_pane_id="$(open_session_window ${session_name})"
 
@@ -187,15 +187,13 @@ function list_sockets() {
   local session_name=$1
 
   gum style --foreground 212 --bold --height 2 "${session_name} Sockman"
-  sleep 5
 
   local new_socket_opt="New Socket"
   local switch_session_opt="Switch Session"
   local close_menu_opt="Close menu"
-  # local sockets="$(socket_list ${session_name})"
+  local sockets="$(socket_list ${session_name})"
 
-  # option="$(gum choose ${sockets} "${new_socket_opt}" "${switch_session_opt}" "${close_menu_opt}")"
-  sleep 5
+  option="$(gum choose ${sockets} "${new_socket_opt}" "${switch_session_opt}" "${close_menu_opt}")"
 }
 
 function open_list_sessions_pane() {
