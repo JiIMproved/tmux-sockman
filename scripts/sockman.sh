14504df94a35116279277be2597610e504958b6e
James Harvey <jamesharvey@jamess-air.lan>
2024-10-14 09:53:05 -0400

    testing

--- scripts/sockman.sh
+++ scripts/sockman.sh
@@ -165,7 +165,7 @@ function open_session_window() {
 function open_list_sockets_pane() {
   session_name=$1
   local pane_name="$(session_options_pane_title ${session_name})"
-  local pane_id="$(tmux list-panes -aF \"#{pane_id}\" -f \"#{==:#{pane_title},"${pane_name}"}\")"
+  local pane_id="$(tmux list-panes -aF \"#{pane_id}\" -f \"#{==:#{pane_title},${pane_name}}\")"
   sleep 5
   local pane_found="$(tmux select-pane -t "${pane_id}" && echo true)"
 
