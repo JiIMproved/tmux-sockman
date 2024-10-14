2b196922f03a1544175ef2dbccaed330ad961ffa
James Harvey <jamesharvey@jamess-air.lan>
2024-10-13 21:31:40 -0400

    testing

--- scripts/sockman.sh
+++ scripts/sockman.sh
@@ -167,26 +167,18 @@ function open_session_window() {
 function open_list_sockets_pane() {
   local session_name=$1
   local pane_name="$(session_options_pane_title ${session_name})"
-  local pane_found="$(tmux select-pane -t "${pane_name}" && echo true)"
+  local pane_found="$(tmux select-pane -t "${pane_name}" && echo true || clear)"
 
   if [[ -z "${pane_found}" ]]; then
-    echo listing sockets...
     local winid="$(tmux new-window -P bash -c 'source '"${CURRENT_DIR}"'/sockman.sh && list_sockets "${session_name}"')"
-    sleep 2
 
-    echo opening session window
     local session_pane_id="$(open_session_window ${session_name})"
-    sleep 2
 
-    echo joining pane
     tmux join-pane -hb -l 40 -t "${session_pane_id}" -s "${winid}"
-    sleep 2
 
-    echo renaming pane
     # rename current pane so it can be found next time
     tmux select-pane -T "${pane_name}"
     tmux set -w allow-rename off
-    sleep 2
   fi
 
   echo "${pane_name}"
