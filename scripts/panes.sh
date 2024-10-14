c50a8b8480ec7c185d8cbf822e945d6ec659a4d2
James Harvey <jamesharvey@jamess-air.lan>
2024-10-14 13:57:44 -0400

    testing

--- scripts/panes.sh
+++ scripts/panes.sh
@@ -154,3 +154,20 @@ function list_sessions() {
     list_sockets "${option}"
   fi
 }
+
+function open_session_window() {
+  local session_name=$1
+  if [[ -z "${session_name}" ]]; then
+    session_name="$(sockman_session)"
+  fi
+
+  if [[ -z "${session_name}" ]]; then
+    read -p "No session name found. Failed to create or select session window. Press enter to continue."
+    return 1
+  fi
+
+  tmux new-window -e DISABLE_AUTO_TITLE=true -Sn "${session_name}" 2> /dev/null
+  tmux select-pane -T "$(session_primary_pane_title ${session_name})" 2> /dev/null
+  tmux setw -g allow-rename off 2> /dev/null
+  echo "$(tmux display-message -p '#{pane_id}')"
+}
--- scripts/sockman.sh
+++ scripts/sockman.sh
@@ -19,20 +19,3 @@ function toggle_menu() {
     open_list_sockets_pane "${session_name}"
   fi
 }
-
-function open_session_window() {
-  local session_name=$1
-  if [[ -z "${session_name}" ]]; then
-    session_name="$(sockman_session)"
-  fi
-
-  if [[ -z "${session_name}" ]]; then
-    read -p "No session name found. Failed to create or select session window. Press enter to continue."
-    return 1
-  fi
-
-  tmux new-window -e DISABLE_AUTO_TITLE=true -Sn "${session_name}" 2> /dev/null
-  tmux select-pane -T "$(session_primary_pane_title ${session_name})" 2> /dev/null
-  tmux setw -g allow-rename off 2> /dev/null
-  echo "$(tmux display-message -p '#{pane_id}')"
-}
