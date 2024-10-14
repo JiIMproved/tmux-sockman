1cca1ad5762bd00640f32b052bac11fb2cd20b4d
James Harvey <jamesharvey@jamess-air.lan>
2024-10-14 12:50:44 -0400

    testing

--- scripts/sockman.sh
+++ scripts/sockman.sh
@@ -16,7 +16,7 @@ function sockman_session() {
     session_name=$(tmux display-message -p '#W')
   fi
 
-  echo "$(session_list)" > ~/stuff2
+  echo "${session_name}" > ~/stuff3
   if [[ "$(session_list)" =~ "(^|[[:space:]])${session_name}($|[[:space:]])" ]]; then
     echo "${session_name}"
   fi
